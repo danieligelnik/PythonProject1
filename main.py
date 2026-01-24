6b118b898e7ba07934b310018578893d8702785a
Vita Preskovsky <vita.preskovsky@gmail.com>
2026-01-24 23:57:01 +0200

    Functions and more functionality 1. Devision of the code to small functions 2. Adding function to display forecast chart 3. Adding function to show daily summary 4. Adding local time

--- main.py
+++ main.py
@@ -1,53 +1,134 @@
+from datetime import datetime, timezone, timedelta
+import altair as alt
+import pandas as pd
 import streamlit as st
 import requests as req
 
-# Sets title of the application
-st.title('Weather App')
 
-name = st.text_input('Enter your city name', '')
-if not name:
+def get_weather(url,city):
+    # Parameters for Weather API past to get request
+    params = {
+            "q": city,
+            "appid": st.secrets["DB_TOKEN"],
+            "units": st.secrets["weather"]["units"],
+            }
+    # Sent request to weather site
+    r = req.get(url, params=params)
+    if r.status_code != 200:
+        st.error("City not found")
+        st.stop()
+    # Get response from weather site in JSON format and parse it
+    return r.json()
+
+def get_forecast(url, city):
+    fr = req.get(forecast_url,
+            params={"q": city, "appid": st.secrets["DB_TOKEN"], "units": st.secrets["weather"]["units"]})
+    if fr.status_code != 200:
+        st.error("City not found")
+        st.stop()
+    return fr.json()
+
+def show_local_time(data,side):
+    # st.metric("Temperature", f"{temp} °C", delta=f"{temp_delta} °C")
+    utc_time = datetime.fromtimestamp(data["dt"], tz=timezone.utc)
+    city_time = utc_time + timedelta(seconds=data["timezone"])
+
+    with side:
+        st.subheader("Local time")
+        st.markdown(
+                f"<div style='font-size:18px'>{city_time.strftime('%d-%m-%Y %H:%M')}</div>",
+                unsafe_allow_html=True,
+        )
+
+
+def show_weather(data,side):
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    wind = data["wind"]["speed"]
+
+    with side:
+        st.subheader("Current Conditions")
+        # Table 3 columns from streamlit
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Temperature (°C)", temp)
+        col2.metric("Humidity (%)", humidity)
+        col3.metric("Wind (m/s)", wind)
+
+def show_forecast(forecast_data):
+    # forecast graph 
+    df = pd.DataFrame({
+        "time": [x["dt_txt"] for x in forecast_data["list"]],
+        "temp": [x["main"]["temp"] for x in forecast_data["list"]],
+        })
+    st.subheader("Forecast for the next 5 days")
+    chart = (
+        alt.Chart(df)
+        .mark_line(point=True)
+        .encode(
+            x=alt.X("time:T", title="Date / Time"),
+            y=alt.Y("temp:Q", title="Temperature (°C)"),
+            tooltip=["time:T", "temp:Q"],
+        )
+        .properties(height=300)
+    )
+
+    st.altair_chart(chart, width='stretch')
+
+def show_daily_conditions(forecast_data):
+    daily = {}
+
+    for x in forecast_data["list"]:
+        day = x["dt_txt"].split(" ")[0]
+        condition = x["weather"][0]
+
+
+        if day not in daily:
+            daily[day] = {
+                    "condition": condition["main"],
+                    "icon": condition["icon"],
+            }
+
+    df = pd.DataFrame(
+            [{"Date": d, "Condition": c} for d, c in daily.items()]
+    )
+    st.subheader("Daily forecast summary")
+
+    cols = st.columns(len(df))
+    for col, (day,info) in zip(cols,daily.items()):
+        with col:
+            st.markdown(f"**{day}**")
+            st.image(f"https://openweathermap.org/img/wn/{info['icon']}@2x.png", width=60)
+            st.write(info["condition"])
+           
+
+# ===================================================== Main ============================
+# Sets title of the application
+
+url = "https://api.openweathermap.org/data/2.5"
+st.title('Travel Guide')
+
+city = st.text_input('Enter your city name', '')
+if not city:
     st.stop()
-if any(ch.isdigit() for ch in name):
+if any(ch.isdigit() for ch in city):
     st.error("City name must not contain numbers")
     st.stop()
 
-if name:
-    st.write(f'Weather in {name}, welcome to the weather app!')
-
-city = name
-url = "https://api.openweathermap.org/data/2.5/weather"
+if city:
+    st.write(f'Weather in {city}, welcome to the weather app!')
 
 
-# api_key = st.secrets["DB_TOKEN"]
-# username = st.secrets["DB_USERNAME"]
-# units = st.secrets["weather"]["units"]
+weather_url = f"{url}/weather"
+data = get_weather(weather_url,city)
+#create row with left and right where 3/4
+#parts of the screen is left and 1/4 is right
+#this is needed for screen rendering by streamlit
+left, right = st.columns([3,1])
+show_weather(data,side=left)
+show_local_time(data,side=right)
 
-# Parameters for Weather API past to get request
-params = {
-    "q": city,
-    "appid": st.secrets["DB_TOKEN"],
-    "units": st.secrets["weather"]["units"],
-}
-
-# Sent request to weather site
-r = req.get(url, params=params)
-if r.status_code != 200:
-    st.error("City not found")
-    st.stop()
-
-# Get response from weather site in JSON format and parse it
-data = r.json()
-temp = data["main"]["temp"]
-humidity = data["main"]["humidity"]
-wind = data["wind"]["speed"]
-
-st.subheader("Current Conditions")
-
-# Table 3 columns from streamlit
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature (°C)", temp)
-col2.metric("Humidity (%)", humidity)
-col3.metric("Wind (m/s)", wind)
-
-# st.metric("Temperature", f"{temp} °C", delta=f"{temp_delta} °C")
+forecast_url = f"{url}/forecast"
+forecast_data = get_forecast(forecast_url, city)
+show_forecast(forecast_data)
+show_daily_conditions(forecast_data)
 
